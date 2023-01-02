898bb15b0008ba32df3272552665791a2b896f46
DrelezTM <72683265+DrelezTM@users.noreply.github.com>
2023-01-02 19:45:20 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, LocalAuth, Buttons, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const moment = require('moment-timezone');
+
+const client = new Client({
+    restartOnAuthFail: true,
+    puppeteer: {
+        headless: true,
+        args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+    },
+    authStrategy: new LocalAuth({ clientId: "client" })
+});
+const config = require('./config/config.json');
+
+client.on('qr', async (qr) => {
+    console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Scan QR dibawah ini : `);
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.clear();
+    console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.nama} is Already!`);
+});
+
+client.initialize();
