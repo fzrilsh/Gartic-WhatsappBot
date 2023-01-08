7fd9a19cc5d39ba086d5b699f0c671303d49a5cb
fzrilsh <fazriloke18@gmail.com>
2023-01-08 07:49:57 +0700

    gartic

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const moment = require('moment-timezone');
-const Gartic = require('./lib/gartic')
+const Gartic = require('./lib/gartic');
+global.gameSections = {}
+
+const isAdmin = (chat, jid) => chat.participants.find(v => v.id._serialized === jid)?.isAdmin
 
 const client = new Client({
     restartOnAuthFail: true,
@@ -14,6 +17,7 @@ const client = new Client({
 const config = require('./config/config.json');
 
 client.on('qr', async (qr) => {
+    console.clear()
     qrcode.generate(qr, { small: true });
     console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Scan the QR : `);
 });
@@ -21,12 +25,13 @@ client.on('qr', async (qr) => {
 client.on('group_join', async (group) => {
     if(group.id.participant == client.info.wid._serialized){
         let chat = await group.getChat();
-        for(let participant of chat.participants) {
-            if(participant.id._serialized === client.info.wid._serialized && !participant.isAdmin) {
-                group.reply(`BOT MUST BE AN ADMIN`);
-                break;
-            }
-        }
+        if(!isAdmin(chat, client.info.wid._serialized)) group.reply("BOT MUST BE AN ADMIN")
+        // for(let participant of chat.participants) {
+        //     if(participant.id._serialized === client.info.wid._serialized && !participant.isAdmin) {
+        //         group.reply(`BOT MUST BE AN ADMIN`);
+        //         break;
+        //     }
+        // }
     }
 })
 
@@ -35,7 +40,10 @@ client.on('message', async (message) => {
     let args = message.body.substring(config.prefix).split(' ');
     switch(args[0]) { 
         case `${config.prefix}open`:
-            let msg = await client.sendMessage(chatId, "Opened games. please enter the game by react this message with any emojis")
+            if(!chatId.endsWith('g.us')) return
+            if(!isAdmin(await message.getChat(), client.info.wid._serialized)) return message.reply("BOT MUST BE AN ADMIN")
+            if(global.gameSections[chatId]) return message.reply(global.gameSections[chatId])
+            let msg = await message.reply("Opened games. please enter the game by react this message with any emojis")
             new Gartic(client, chatId, msg.id)
         break;
     }
