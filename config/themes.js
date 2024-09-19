281eb870ee7b5bc946acc9c17c5f3a8d9cff6ecc
fzrilsh <fazriloke18@gmail.com>
2024-09-19 15:55:44 +0700

    add more theme

--- config/themes.js
+++ config/themes.js
@@ -1,6 +1,122 @@
 module.exports = [
     {
-        name: "lebaran",
-        object: ["ketupat", "ied"]
+        "name": "hewan",
+        "object": ["kucing", "gajah", "elang", "ikan", "harimau"]
+    },
+    {
+        "name": "buah",
+        "object": ["apel", "jeruk", "mangga", "pisang", "anggur"]
+    },
+    {
+        "name": "transportasi",
+        "object": ["mobil", "pesawat", "kereta", "sepeda", "kapal"]
+    },
+    {
+        "name": "olahraga",
+        "object": ["sepak bola", "basket", "renang", "badminton", "tenis"]
+    },
+    {
+        "name": "pekerjaan",
+        "object": ["dokter", "guru", "polisi", "programmer", "pilot"]
+    },
+    {
+        "name": "makanan",
+        "object": ["nasi goreng", "pizza", "sate", "bakso", "burger"]
+    },
+    {
+        "name": "negara",
+        "object": ["Indonesia", "Jepang", "Perancis", "Brazil", "Mesir"]
+    },
+    {
+        "name": "alat musik",
+        "object": ["gitar", "drum", "piano", "biola", "seruling"]
+    },
+    {
+        "name": "benda di rumah",
+        "object": ["televisi", "sofa", "lemari", "meja", "kursi"]
+    },
+    {
+        "name": "warna",
+        "object": ["merah", "biru", "kuning", "hijau", "ungu"]
+    },
+    {
+        "name": "kendaraan umum",
+        "object": ["bus", "taksi", "angkot", "ojek", "kereta api"]
+    },
+    {
+        "name": "binatang laut",
+        "object": ["hiu", "paus", "ubur-ubur", "ikan pari", "penyu"]
+    },
+    {
+        "name": "serangga",
+        "object": ["nyamuk", "kupu-kupu", "lebah", "semut", "jangkrik"]
+    },
+    {
+        "name": "cuaca",
+        "object": ["cerah", "hujan", "berawan", "badai", "salju"]
+    },
+    {
+        "name": "planet",
+        "object": ["Merkurius", "Venus", "Bumi", "Mars", "Saturnus"]
+    },
+    {
+        "name": "tokoh superhero",
+        "object": ["Superman", "Batman", "Spider-Man", "Wonder Woman", "Iron Man"]
+    },
+    {
+        "name": "alat tulis",
+        "object": ["pensil", "pulpen", "penghapus", "penggaris", "spidol"]
+    },
+    {
+        "name": "peralatan dapur",
+        "object": ["pisau", "sendok", "garpu", "wajan", "kompor"]
+    },
+    {
+        "name": "film",
+        "object": ["Titanic", "Avengers", "Harry Potter", "Inception", "Frozen"]
+    },
+    {
+        "name": "musik",
+        "object": ["jazz", "pop", "rock", "hip hop", "klasik"]
+    },
+    {
+        "name": "pakaian",
+        "object": ["kaos", "celana", "jaket", "topi", "sepatu"]
+    },
+    {
+        "name": "alat kebersihan",
+        "object": ["sapu", "kain pel", "sikat", "ember", "pengki"]
+    },
+    {
+        "name": "hewan peliharaan",
+        "object": ["anjing", "kucing", "burung", "kelinci", "ikan"]
+    },
+    {
+        "name": "sayuran",
+        "object": ["bayam", "wortel", "kentang", "brokoli", "tomat"]
+    },
+    {
+        "name": "peralatan olahraga",
+        "object": ["bola", "raket", "sepatu lari", "helm", "matras"]
+    },
+    {
+        "name": "alat elektronik",
+        "object": ["komputer", "telepon", "kulkas", "microwave", "kamera"]
+    },
+    {
+        "name": "jenis musik",
+        "object": ["rock", "pop", "jazz", "blues", "reggae"]
+    },
+    {
+        "name": "permainan",
+        "object": ["catur", "monopoli", "kartu", "puzzle", "ular tangga"]
+    },
+    {
+        "name": "bunga",
+        "object": ["mawar", "melati", "anggrek", "tulip", "teratai"]
+    },
+    {
+        "name": "tanaman",
+        "object": ["pohon mangga", "kaktus", "bambu", "pohon kelapa", "bonsai"]
     }
 ]
