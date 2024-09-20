a0258b218c9163f2ac53e339f041b6532f36b37e
Fazril <fazriloke18@gmail.com>
2024-09-20 13:44:27 +0700

    add checking similarity of answer; change: if all players already answer, game turn out

--- Gartic.js
+++ Gartic.js
@@ -1,5 +1,5 @@
 const { randomTheme, validateAnswer } = require("./lib/Utils/theme");
-const { clockString, isGroup } = require("./lib/Utils/Util");
+const { clockString, isGroup, stringSimilarity } = require("./lib/Utils/Util");
 const messages = require('./config/messages.json');
 const { MessageMedia } = require("whatsapp-web.js");
 const sections = []
@@ -31,7 +31,7 @@ class Gartic {
         this.theme = null
         this.timeStartedGuess = null
         this.state = this.WAITING
-        this.caches = { messages: [], message_reactions: [] }
+        this.caches = { messages: [], message_reactions: [], timeout: {} }
 
         this.recrutPlayer()
     }
@@ -40,8 +40,8 @@ class Gartic {
         return sections.find(v => v.id === id)
     }
 
-    removeEvent(eventName, id){
-        global[eventName+'Event'].splice(global[eventName+'Event'].findIndex(event => event.id === id), 1)
+    removeEvent(eventName, id) {
+        global[eventName + 'Event'].splice(global[eventName + 'Event'].findIndex(event => event.id === id), 1)
     }
 
     destroy() {
@@ -49,7 +49,7 @@ class Gartic {
         this.state = this.END
 
         global.messageEvent.forEach((v, i) => {
-            if(v.classId === this.id) global.messageEvent.splice(i, 1)
+            if (v.classId === this.id) global.messageEvent.splice(i, 1)
         })
     }
 
@@ -57,19 +57,18 @@ class Gartic {
         return this.players.find(playerID => playerID === id)
     }
 
-    resetPlayer(){
+    resetPlayer() {
         this.players = this.players.map(player => player.replace('-true', ''))
     }
 
-    checkGameover(){
+    checkGameover() {
         let scores = Object.values(this.score).find(v => v >= 500)
         let formattedScore = Object.keys(this.score).map(v => ({ id: v, score: this.score[v] }))
 
-        this.client.sendMessage(this.id, 'Game Over')
-        console.log(formattedScore)
-
-        if(scores) return true
-        else return false
+        if (scores) {
+            this.client.sendMessage(this.id, 'Game Over')
+            return true
+        }
     }
 
     recrutPlayer() {
@@ -101,13 +100,13 @@ class Gartic {
             if (this.state !== this.END) this.players.push(react.senderId)
             if (this.state === this.GUESSES) this.queue.push(react.senderId)
 
-            this.client.sendMessage(this.id, messages.player_joined.replace(':tag', react.senderId.split('@')[0]), { mentions: [react.senderId] })
+            if (this.state !== this.END) this.client.sendMessage(this.id, messages.player_joined.replace(':tag', react.senderId.split('@')[0]), { mentions: [react.senderId] })
         })
     }
 
     turnDraw() {
-        if(this.checkGameover()) return false
-        
+        if (this.checkGameover()) return false
+
         this.theme = randomTheme()
         const id = Date.now().toString(14)
 
@@ -115,7 +114,7 @@ class Gartic {
         else this.queueTurn++
 
         this.queue.forEach(player => {
-            if(!(player in this.score)) this.score[player] = 0
+            if (!(player in this.score)) this.score[player] = 0
         })
 
         this.playerTurn = this.queue[this.queueTurn]
@@ -124,8 +123,8 @@ class Gartic {
         this.state = this.STARTED
         this.client.sendMessage(this.playerTurn, messages.choose_object.replace(':theme', this.theme.name).replace(':options', this.theme.object.map((v, i) => '\n' + (i + 1) + '. ' + v.toUpperCase()).join('')))
         setTimeout(() => {
-            if(this.state === this.END) return false
-            if(this.state === this.STARTED){
+            if (this.state === this.END) return false
+            if (this.state === this.STARTED) {
                 global.messageEvent.splice(global.messageEvent.findIndex(v => v.id === id), 1)
                 this.resetPlayer()
 
@@ -137,42 +136,56 @@ class Gartic {
         /**
          * @param {import('whatsapp-web.js').Message} message 
          */
-        let messageHandler = async(message) => {
-            if(typeof(this.theme.object) === 'object' && message.from === this.playerTurn){
+        let messageHandler = async (message) => {
+            if (typeof (this.theme.object) === 'object' && message.from === this.playerTurn) {
                 let selected = parseInt(message.body) - 1 in this.theme.object ? this.theme.object[parseInt(message.body) - 1] : this.theme.object.find(v => v === message.body.toLowerCase()) ? this.theme.object.find(v => v === message.body.toLowerCase()) : false
                 if (!selected) return false
 
                 this.state = this.GUESSES
                 this.theme.object = selected
-                this.client.sendMessage(this.playerTurn, MessageMedia.fromFilePath('./lib/img/blank.png'), {caption: messages.object_choosen.replaceAll(':object', selected.toUpperCase())})
+                this.client.sendMessage(this.playerTurn, MessageMedia.fromFilePath('./lib/img/blank.png'), { caption: messages.object_choosen.replaceAll(':object', selected.toUpperCase()) })
                 this.client.sendMessage(this.id, messages.guess_drawed.replace(':tag', this.playerTurn.split('@')[0]), { mentions: [this.playerTurn] })
 
                 this.timeStartedGuess = + new Date()
-                setTimeout(() => {
-                    if(this.state === this.END) return false
-        
-                    this.client.sendMessage(this.id, messages.guess_timeout_1_minute_left)
-                    setTimeout(() => {
-                        if(this.state === this.END) return false
-                        
-                        global.messageEvent.splice(global.messageEvent.findIndex(v => v.id === id), 1)
-                        this.resetPlayer()
+                this.caches.timeout[id] = setTimeout(() => {
+                    if (this.state === this.END) return false
 
-                        this.client.sendMessage(this.id, messages.guess_timeout.replace(':object', this.theme.object.toUpperCase()))
-                        this.client.sendMessage(this.id, "*SCORE SEMENTARA*\n" + (Object.keys(this.score).sort((a, b) => this.score[a] + this.score[b]).map((v, i) => {
-                            return `[${i + 1}] _@${v.split('@')[0]}_ : ${this.score[v]} Point`
-                        }).join('\n')), { mentions: this.queue })
+                    global.messageEvent.splice(global.messageEvent.findIndex(v => v.id === id), 1)
+                    this.resetPlayer()
 
-                        setTimeout(this.turnDraw.bind(this), 5000);
-                    }, 1000 * 60);
-                }, 1000 * 60 * 2);
+                    this.client.sendMessage(this.id, messages.guess_timeout.replace(':object', this.theme.object.toUpperCase()))
+                    this.client.sendMessage(this.id, "*SCORE SEMENTARA*\n" + (Object.keys(this.score).sort((a, b) => this.score[a] + this.score[b]).map((v, i) => {
+                        return `[${i + 1}] _@${v.split('@')[0]}_ : ${this.score[v]} Point`
+                    }).join('\n')), { mentions: this.queue })
+
+                    setTimeout(this.turnDraw.bind(this), 5000);
+                }, 1000 * 60 * 3);
+
+                // setTimeout(() => {
+                //     if(this.state === this.END) return false
+
+                //     this.client.sendMessage(this.id, messages.guess_timeout_1_minute_left)
+                //     setTimeout(() => {
+                //         if(this.state === this.END) return false
+
+                //         global.messageEvent.splice(global.messageEvent.findIndex(v => v.id === id), 1)
+                //         this.resetPlayer()
+
+                //         this.client.sendMessage(this.id, messages.guess_timeout.replace(':object', this.theme.object.toUpperCase()))
+                //         this.client.sendMessage(this.id, "*SCORE SEMENTARA*\n" + (Object.keys(this.score).sort((a, b) => this.score[a] + this.score[b]).map((v, i) => {
+                //             return `[${i + 1}] _@${v.split('@')[0]}_ : ${this.score[v]} Point`
+                //         }).join('\n')), { mentions: this.queue })
+
+                //         setTimeout(this.turnDraw.bind(this), 5000);
+                //     }, 1000 * 60);
+                // }, 1000 * 60 * 2);
             }
 
-            if(isGroup(message.from) && message.from === this.id && this.state === this.GUESSES){
-                if(validateAnswer(this.theme, message.body)){
+            if (isGroup(message.from) && message.from === this.id && this.state === this.GUESSES) {
+                if (validateAnswer(this.theme, message.body)) {
                     message.delete(true)
 
-                    if(this.players.includes(message.author) && message.author !== this.playerTurn){
+                    if (this.players.includes(message.author) && message.author !== this.playerTurn) {
                         let secondAnswered = clockString(new Date() - this.timeStartedGuess)[2]
                         let point = (secondAnswered < 10 ? 20 : secondAnswered < 18 ? 18 : secondAnswered < 28 ? 15 : 10)
 
@@ -181,7 +194,14 @@ class Gartic {
 
                         this.players[this.players.findIndex(v => v === message.author)] += '-true'
                         this.client.sendMessage(this.id, messages.correct_answer.replace(':tag', message.author.split('@')[0]).replace('score', point), { mentions: [message.author] })
+
+                        if (this.players.filter(v => v.includes('-true')).length === this.players.length - 2) {
+                            this.caches.timeout[id]._onTimeout()
+                            clearTimeout(this.caches.timeout[id])
+                        }
                     }
+                }else if(stringSimilarity(message.body, this.theme) > 0.6){
+                    message.reply('[!] Jawaban kamu hampir benar!')
                 }
             }
         }
